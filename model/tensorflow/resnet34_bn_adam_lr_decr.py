ca855299ea8ab69336d10866509222569fd679d3
Allen <allenlee@mit.edu>
2017-11-20 14:22:49 -0500

    adding save point

--- model/tensorflow/resnet34_bn_adam_lr_decr.py
+++ model/tensorflow/resnet34_bn_adam_lr_decr.py
@@ -19,7 +19,7 @@ training_iters = 4500
 step_display = 50
 step_save = 1500
 path_save = 'resnet_34_adam_bn_adjusted/'
-start_from = ''
+start_from = 'True'
 
 if not os.path.exists(path_save):
     os.makedirs(path_save)

fa8379b0e0dea83d9b2b5b3b351d173710f0fe98
Allen <allenlee@mit.edu>
2017-11-21 02:08:46 -0500

    retrain

--- model/tensorflow/resnet34_bn_128.py
+++ model/tensorflow/resnet34_bn_128.py
@@ -6,7 +6,7 @@ from DataLoader import *
 from DataLoader_test import *
 
 # Dataset Parameters
-batch_size = 64
+batch_size = 128
 load_size = 128
 fine_size = 112 # image size, 112x112x3
 c = 3
@@ -15,10 +15,10 @@ data_mean = np.asarray([0.45834960097,0.44674252445,0.41352266842])
 # Training Parameters
 learning_rate = 0.0001
 # dropout = 0.55 # Dropout, probability to keep units
-training_iters = 22000
+training_iters = 20000
 step_display = 50
 step_save = 1000
-path_save = 'resnet_34_22k_bn/'
+path_save = 'resnet_34_128_bn/'
 start_from = ''
 
 if not os.path.exists(path_save):
